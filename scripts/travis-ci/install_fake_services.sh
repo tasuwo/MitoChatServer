1b3492a646c9832b74f3585ca62fe5f038265410
tasuwo <kamuhata.you@gmail.com>
2018-07-15 15:14:48 +0900

    :white_check_mark: Fakeサービスを利用してAWSリソース連携をテストする
    fakes3, dynamodb local を AWS SAM と組み合わせて利用し、
    ローカル/TravisCI上での統合テストを実現する

--- scripts/travis-ci/install_fake_services.sh
+++ scripts/travis-ci/install_fake_services.sh
@@ -4,4 +4,8 @@
 # 統合テスト実行に必要な fake services のインストール
 #
 
+mkdir dynamodb_local
+curl https://s3-ap-northeast-1.amazonaws.com/dynamodb-local-tokyo/dynamodb_local_latest.zip > dynamodb_local/dynamodb_local.zip
+unzip dynamodb_local/dynamodb_local.zip -d dynamodb_local
+gem install fakes3 -v 1.2.1
 pip install --user aws-sam-cli==0.4.0
